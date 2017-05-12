a1fbc8c0af458b66816e1c5a285c1bc06dc67b8d
Casey Kelly <casey.kelly@dish.com>
2017-05-11 22:58:41 -0600

    import JamList container into app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import JamList from '../containers/jam-list'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <JamList />
+      </div>
     );
   }
 }
