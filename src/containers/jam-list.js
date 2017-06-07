de45b6278848e729d3e9d03b96757010afc35aeb
Casey Kelly <casey.kelly@dish.com>
2017-06-06 22:59:56 -0600

    add comments to JamList container to explain map to props functions

--- src/containers/jam-list.js
+++ src/containers/jam-list.js
@@ -22,13 +22,24 @@ class JamList extends Component {
 }
 
 function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of JamList
+
   return {
     jams: state.jams
   }
 }
 
+// Anything returned from this function will end up as props
+// on the JamList container
 function mapDispatchToProps(dispatch) {
+  // Whenever selectJam is called, the result should
+  // be passed to all of our reducers
+
   return bindActionCreators({ selectJam: selectJam }, dispatch);
 }
 
+// Promote JamList from a component to a container - it
+// needs to know about this new dispatch method, selectJam.
+// Make it available as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(JamList);
