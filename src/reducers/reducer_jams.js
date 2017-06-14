c812bba9a2697952bef14e6315b64ead4a6e7139
Casey Kelly <casey.kelly@dish.com>
2017-06-14 14:40:17 -0600

    add date prop to jams reducer

--- src/reducers/reducer_jams.js
+++ src/reducers/reducer_jams.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: "Tweezer" },
-    { title: "Bathtub Gin" },
-    { title: "Chalkdust Torture" },
-    { title: "Down With Disease" }
+    { title: "Tweezer", date: "11/17/1997" },
+    { title: "Bathtub Gin", date: "8/10/1997" },
+    { title: "Chalkdust Torture", date: "7/10/1999" },
+    { title: "Down With Disease", date: "12/31/1995" }
   ]
 }
